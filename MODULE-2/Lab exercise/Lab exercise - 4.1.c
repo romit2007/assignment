505699e45d696199da6359e3aa517f0ef9078f37
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 10:39:51 +0530

    circumfence

--- MODULE-2/Lab exercise/Lab exercise - 4.1.c
+++ MODULE-2/Lab exercise/Lab exercise - 4.1.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    float PI,r,area;
+    float PI,r,area,circumfence;
 
     printf("\n Enter radious of circcle: ");
     scanf("%f",&r);
 
     area=PI*r*r;
     printf("\n Area of circle : %f",area);
+
+    circumfence=2*PI*r;
+    printf("\n Circumfence of circle :%f",circumfence);
     
 }
+
