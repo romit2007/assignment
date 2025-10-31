4ee657c92eae2dd3d3f88d12f57696d7438206ba
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 09:22:18 +0530

    Refactor Armstrong number program for single and range checks
    The program now checks if a single integer is an Armstrong number and also lists all Armstrong numbers between 1 and 1000.

--- MODULE-2/EXTRA LOGIC BUILDING PROGRAM/armstong number.c
+++ MODULE-2/EXTRA LOGIC BUILDING PROGRAM/armstong number.c
@@ -3,46 +3,65 @@
 
 int main()
 {
-    int start, end;
+    int num, originalNum, remainder, n = 0;
+    double result = 0.0;
 
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
-    printf("Enter  start: ");
-    scanf("%d", &start);
-
-     printf("Enter end: ");
-    scanf("%d", &end);
-
-
-    printf("\nArmstrong numbers between %d and %d are:\n", start, end);
-
-    for (int num = start; num <= end; num++)
-    {
-        int ogNum = num, rem, p = 0;
-        double sum = 0.0;
-
-
-
-    while (ogNum != 0)
-    {
-        ogNum = ogNum / 10;
-        p++;
-    }
-
-    ogNum = num; 
+    originalNum = num;
 
     
-    while (ogNum > 0)
+    while (originalNum != 0)
     {
-        rem = ogNum % 10;
-        sum += pow(rem, p);
-        ogNum = ogNum / 10;
+        originalNum /= 10;
+        ++n;
     }
 
+    originalNum = num;
 
-    if ((int)sum == num)
+    // Compute the Armstrong sum
+    while (originalNum != 0)
+    {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    if ((int)result == num)
         printf("%d is an Armstrong number.\n", num);
     else
         printf("%d is not an Armstrong number.\n", num);
+
+    
+    printf("\nArmstrong numbers between 1 and 1000 are:\n");
+
+    for (int i = 1; i <= 1000; ++i)
+    {
+        int temp = i, digits = 0, rem;
+        double sum = 0.0;
+
+        // Count digits
+        while (temp != 0)
+        {
+            temp /= 10;
+            ++digits;
+        }
+
+        temp = i;
+
+
+        while (temp != 0)
+        {
+            rem = temp % 10;
+            sum += pow(rem, digits);
+            temp /= 10;
+        }
+
+        if ((int)sum == i)
+            printf("%d ", i);
     }
+
+    printf("\n");
     return 0;
 }
