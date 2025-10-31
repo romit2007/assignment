0456c77aa7d96b7e7ecceb7290c1f64919508429
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 10:51:37 +0530

    Add files via upload

---
+++ MODULE-2/Lab exercise/lab exercise-4.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,ans;
+
+    printf("\n Enter number: ");
+    scanf("%d",&n);
+
+   ans=5*n;
+   printf("\n need %d apples for %d students",ans,n);
+
+
+
+    return 0;
+}
