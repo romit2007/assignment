ff900a4672350b3c170552514a03c1dc3d456449
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 11:42:14 +0530

    set range
    Added functionality to specify multiplication range.

--- MODULE-2/LOGIC BUILDING PROGRAM/table.c
+++ MODULE-2/LOGIC BUILDING PROGRAM/table.c
@@ -2,16 +2,20 @@
 #include<conio.h>
 int main()
 {
-    int num,i,ans;
+    int num,i,ans,range;
 
     printf("\n Enter a number: ");
     scanf("%d",&num);
+    
+    printf("\n Enter range: ");
+    scanf("%d",&range);
 
-    for(i=1;i<=10;i++)
+    for(i=1;i<=range;i++)
     {
         ans = num * i;
         printf("\n %d x %d = %d",num,i,ans);
     }
     
+
 
 }
