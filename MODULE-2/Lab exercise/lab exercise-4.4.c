f6f7bb87badcf70d06508d708aaf0d8054b5aaaf
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 11:01:05 +0530

    Add files via upload

---
+++ MODULE-2/Lab exercise/lab exercise-4.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ascii,ch;
+
+    printf("\n Enter character: ");
+    scanf("%c",&ch);
+
+    ascii=ch;
+    printf("\n ASCII value of %c is %d",ch,ascii);
+
+
+
+  
+    return 0;
+
+}
---
+++ MODULE-2/Lab exercise/lab exercise-4.5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int ascii,ch;
+
+    printf("\n Enter character: ");
+    scanf("%c",&ch);
+
+    ascii=ch;
+    printf("\n ASCII value of %c is %d",ch,ascii);
+    
+}
