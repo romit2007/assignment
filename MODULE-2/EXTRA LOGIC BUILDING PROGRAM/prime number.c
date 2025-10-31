cf47086c3cd3bcc3c2d27bd6807f2091eda0d3d3
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 11:33:27 +0530

    p

--- MODULE-2/EXTRA LOGIC BUILDING PROGRAM/prime number.c
+++ MODULE-2/EXTRA LOGIC BUILDING PROGRAM/prime number.c
@@ -1,36 +1,40 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
+int prime(int num)
 {
-    int num;
-
-    printf("\n Enter number: ");
-    scanf("%d",&num);
-
     if(num<=1)
     {
-        printf("\n Not a prime number");
+        return 0; 
     }
     else
     {
-        int Primenumber = 1;
-
         for(int i=2; i<=num/2; i++)
         {
             if(num%i == 0)
             {
-                Primenumber = 0; 
-                break;
+                return 0; 
             }
         }
+        return 1; 
+    }
+}
+int main()
+{
+    int n,i,sum=0;
 
-        if(Primenumber)
+    printf("\n Enter number: ");
+    scanf("%d",&n);
+
+    for(i=2;i<=n;i++)
+    {
+        if(prime(i))
         {
-            printf("\n %d is a prime number", num);
-        }
-        else
-        {
-            printf("\n %d is not a prime number", num);
+            printf("\n %d",i);
+            sum+=i;
         }
     }
+
+    printf("\n sum of prime number : %d",sum);
+
+
 }
