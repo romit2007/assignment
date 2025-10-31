47c85d4a518e71bc50f196a80f8ae7987168e019
romit2007 <romitvaghela2007@gmail.com>
2025-10-31 11:46:35 +0530

    rev

--- MODULE-2/LOGIC BUILDING PROGRAM/sum of digits.c
+++ MODULE-2/LOGIC BUILDING PROGRAM/sum of digits.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 int main()
 {
-    int num, rem = 0, sum = 0;
+    int num, rem = 0, sum = 0,rev=0;
     
     printf("Enter any number: ");
     scanf("%d", &num);
@@ -11,10 +11,12 @@ int main()
     {
         rem = num % 10;
         sum += rem;
+        rev=rev*10+rem;
         num = num / 10;
     }
     
     printf("Sum of all digits: %d", sum);
+    printf("\n Reversed digit: %d",rev);
     
     
     return 0;
